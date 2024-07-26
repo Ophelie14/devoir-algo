9f0c5693d0e5a3015c2723185626802e3eee45a5
Ophelie14 <xx-ophel14@hotmail.fr>
2024-07-26 15:11:22 +0200

    Génération de la recette

--- devoir.php
+++ devoir.php
@@ -7,9 +7,7 @@
                     "beurre" => 100,
                     "sucre" => 80,
                     ];
-
-
-        
+   
      $_actions = [
                     "Coupez" => 800,
                     "mélangez" => 500,
@@ -17,7 +15,10 @@
                     ];
 
  // Demander à l’utilisateur :
+    $_demande = readline ("Donnez un ingrédient supplémentaire : ");
 
- $demande = ("Donnez un ingrédient supplémentaire : "); 
+//  Génération de la recette :
+    $recette = "";
+
 
 ?>
