04eada261769f1d606f87a12ad558f0f80e5b21b
Ophelie14 <xx-ophel14@hotmail.fr>
2024-07-26 16:29:34 +0200

    Finalisation de la recette

--- devoir.php
+++ devoir.php
@@ -1,14 +1,14 @@
 <?php
 
 // <!-- Préparation des données : -->
-    $_ingredients = [
+    $_ingredients= [
                     "farine" => 800,
                     "lait" => 500,
                     "beurre" => 100,
                     "sucre" => 80,
                     ];
    
-     $_actions = [
+     $_actions= [
                     "Coupez" => 800,
                     "mélangez" => 500,
                     "faites revenir" => 100,
@@ -23,15 +23,19 @@
     $title= "Recette de gâteau nature \n";
 
 // Traitement des ingrédients :
+    in_array("Coupez", $_actions);
+
     foreach ($_ingredients as $ingredient => $grammes) {
         echo "Votre gâteau contient $grammes grammes de $ingredient.\n";
     }
 
-    echo " $_actions[1] le $_ingredients[2] \n";
-    echo " $_actions[2] le $_ingredients[3] \n";
-    echo " $_actions[1] le $_ingredients[0] et le $_ingredients[1]  \n";
+    echo " $_actions[1] le $_ingredients[2]\n";
+    echo " $_actions[2] le $_ingredients[3]\n";
+    echo " $_actions[1] le $_ingredients[0] et le $_ingredients[1]\n";
 
-    // Finalisation de la recette :
+
+// Finalisation de la recette :
+echo " Servir chaud et déguster !.\n"
 
 
 ?>
