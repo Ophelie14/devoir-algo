8ad23113a3a7fd549a05257d0772d128cdf4b61a
Ophelie14 <xx-ophel14@hotmail.fr>
2024-07-26 14:49:42 +0200

    Exercice 1

--- devoir.php
+++ devoir.php
@@ -1,1 +1,13 @@
-Hello world
+<?php
+
+// <!-- Préparation des données : -->
+    $ingredients = [
+                    "farine" => 800,
+                    "lait" => 500,
+                    "beurre" => 100,
+                    "sucre" => 80,
+                    ];
+
+
+
+?>
